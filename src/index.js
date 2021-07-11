a2e9fc2929d396474503c0f836ffe13b592f753f
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-07-11 19:28:56 -0400

    add service worker and get PWA badge in lighthouse

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "assets/css/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -18,3 +19,7 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
