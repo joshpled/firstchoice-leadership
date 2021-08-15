fbdc7b54b6605f5edd4dd519755637806ca7d729
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-08-15 19:29:03 -0400

    Customize 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import lost from "../assets/images/Lost.png";
 
-const NotFound = () => (
-  <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-    <h1 className="display-1 " style={{ color: "white" }}>
-      You might be lost!
-    </h1>
-    <img src={lost} style={{ height: "200px", margin: "20px" }} />
-    <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-      <Button style={{ width: "200px" }}>Go Home</Button>
-    </Link>
-  </div>
-);
+const NotFound = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+      <h1 className="display-1 " style={{ color: "white" }}>
+        You might be lost!
+      </h1>
+      <img src={lost} style={{ height: "200px", margin: "20px" }} alt="lost" />
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+        <Button style={{ width: "200px" }}>Go Home</Button>
+      </Link>
+    </div>
+  );
+};
 
 export default NotFound;
